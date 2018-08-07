7f0f850a2d3f003fbe77c7fca3d7a840b38c82c1
Jez <jezaki48@gmail.com>
2018-08-07 16:32:55 -0500

    Version 2
    Added file-output, optimized functions for making calculations and displaying in the output file.

--- Net-Worth-Calculator-master/pro_gui.hpp
+++ Net-Worth-Calculator-master/pro_gui.hpp
@@ -1,6 +1,6 @@
 /*
-Name: Archit Kalpeshkumar Jaiswal
-UTA ID: 1001543326
+Name: Archit Kalpeshkumar Jaiswal/Michael Jezreel Aquitania
+UTA ID: 1001543326/1001542979
 HW5
 */
 #ifndef _CLOSET_FUNC_C
@@ -16,22 +16,59 @@ using namespace Gtk;
 
 //creates dialog and prompt messages
 
+class Person
+{
+
+public:
+	vector<string> liabilities;
+	vector<string> assets;
+	string name;
+	double income;
+
+	void fillLiaAss()
+	{
+		liabilities.push_back("Electricity Bill: $");
+		liabilities.push_back("Water Bill: $");
+		liabilities.push_back("Gas Bill: $");
+		liabilities.push_back("Internet Bill: $");
+		liabilities.push_back("Insurance Premium: $");
+		liabilities.push_back("Phone bill: $");
+		liabilities.push_back("Loans Installment: $");
+
+		assets.push_back("Cash in Hand: $");
+		assets.push_back("Checking Account: $");
+		assets.push_back("Savings Account: $");
+		assets.push_back("Brokerage Accounts (stocks,etc.): $");
+		assets.push_back("Primary Residence: $");
+		assets.push_back("Vehicle: $");
+		assets.push_back("Other: $");
+	}	
+
+};
 
 
 class liability
 {
 public:
-  double ele;
+ /* double ele;
   double water;
   double gas;
   double wifi;
   double phone;
   double insurance;
-  double loansOther;
+  double loansOther;*/
+
+	vector <double> liaValue;
 
 	double total_lia()
 	{
-		double total = ele+water+gas+wifi+phone+insurance+loansOther;
+		/*double total = ele+water+gas+wifi+phone+insurance+loansOther;*/
+		
+		double total = 0;
+		for(int i = 0; i< liaValue.size();i++)
+		{
+			total = total + liaValue[i];
+		}
 		return total;
 	}
 
@@ -40,24 +77,26 @@ public:
 class Assets
 {
 public:
-	//vector <string> other;
-	//vector<double> otherF;
+	/*vector <string> other;
+	vector<double> otherF;
 	double cashOnHand;
 	double checkingAccounts;
 	double savingsAccounts;
 	double MoneyMarkets;
 	double PrimaryResidence;
 	double vehicle;
-	double other;
+	double other;*/
+
+	vector <double> assValue;
 
 	double total_assets()
 	{
-		/*double otherT=0;
-		for(int i = 0; i<other.size();i++)
+		double total=0;
+		for(int i = 0; i<assValue.size();i++)
 		{
-			otherT = otherT+otherF[i];
-		}*/
-		double total = cashOnHand+checkingAccounts+savingsAccounts+MoneyMarkets+PrimaryResidence+vehicle+other;
+			total = total+assValue[i];
+		}
+		//double total = cashOnHand+checkingAccounts+savingsAccounts+MoneyMarkets+PrimaryResidence+vehicle+other;
 		return total;
 	}
 
@@ -91,17 +130,16 @@ public:
 //accepts user inputs
    static string * input(string title, string default_text="")
   {
+	    Gtk::Label *lab[7];
+	    Gtk::Entry *ent[7];
 
 	    Gtk::Dialog *dialog = new Gtk::Dialog();
 	    dialog->set_title(title);
 
-
-	    Gtk::Label *lab[7];
-	    Gtk::Entry *ent[7];
 	    //Gtk::Label *label = new Gtk::Label(msg);
 
 		//electricity
-		lab[0] = new Gtk::Label("How much was you last month's electricity bill? ");
+	   lab[0] = new Gtk::Label("Electricity Bill: ");
 	    ent[0] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[0]);
 	    lab[0]->show();
@@ -111,7 +149,7 @@ public:
 	    dialog->get_vbox()->pack_start(*ent[0]);
 
 	    //water
-	    lab[1] = new Gtk::Label("How much was you last month's water bill? ");
+	    lab[1] = new Gtk::Label("Water Bill: ");
 	    ent[1] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[1]);
 	    lab[1]->show();
@@ -121,7 +159,7 @@ public:
 	    dialog->get_vbox()->pack_start(*ent[1]);
 
 	    //gas
-	    lab[2] = new Gtk::Label("How much was you last month gas bill? ");
+	    lab[2] = new Gtk::Label("Gas Bill: ");
 	    ent[2] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[2]);
 	    lab[2]->show();
@@ -131,7 +169,7 @@ public:
 	    dialog->get_vbox()->pack_start(*ent[2]);
 
 	    //wifi
-	    lab[3] = new Gtk::Label("How much was you last month's Internet bill? ");
+	    lab[3] = new Gtk::Label("Internet Bill: ");
 	    ent[3] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[3]);
 	    lab[3]->show();
@@ -141,7 +179,7 @@ public:
 	    dialog->get_vbox()->pack_start(*ent[3]);
 
 	    //phone
-	    lab[4] = new Gtk::Label("How much was you last month's phone bill? ");
+	    lab[4] = new Gtk::Label("Phone bill: ");
 	    ent[4] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[4]);
 	    lab[4]->show();
@@ -151,7 +189,7 @@ public:
 	    dialog->get_vbox()->pack_start(*ent[4]);
 
 	    //insurance
-	    lab[5] = new Gtk::Label("How much was you last month's insurance premium? ");
+	    lab[5] = new Gtk::Label("Insurance Premium: ");
 	    ent[5] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[5]);
 	    lab[5]->show();
@@ -161,7 +199,7 @@ public:
 	    dialog->get_vbox()->pack_start(*ent[5]);
 
 	    //loans
-	    lab[6] = new Gtk::Label("How much was you last month's loans instalment? ");
+	    lab[6] = new Gtk::Label("Loans Installment: ");
 	    ent[6] = new Gtk::Entry();
 	    dialog->get_content_area()->pack_start(*lab[6]);
 	    lab[6]->show();
@@ -218,82 +256,83 @@ public:
 
 static string * inputAssets(string title, string default_text="")
 {
+
+	Gtk::Label *lab1[7];
+	 Gtk::Entry *ent1[7];
+
 	Gtk::Dialog *dialog = new Gtk::Dialog();
 	    dialog->set_title(title);
 
-
-	    Gtk::Label *lab[7];
-	    Gtk::Entry *ent[7];
 	    //Gtk::Label *label = new Gtk::Label(msg);
 
 
-	   lab[0] = new Gtk::Label("Cash in Hand: ");
-	    ent[0] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[0]);
-	    lab[0]->show();
-	    ent[0]->set_text(default_text);
-	    ent[0]->set_max_length(50);
-	    ent[0]->show();
-	    dialog->get_vbox()->pack_start(*ent[0]);
+	   lab1[0] = new Gtk::Label("Cash in Hand: ");
+	    ent1[0] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[0]);
+	    lab1[0]->show();
+	    ent1[0]->set_text(default_text);
+	    ent1[0]->set_max_length(50);
+	    ent1[0]->show();
+	    dialog->get_vbox()->pack_start(*ent1[0]);
 
 
-	    lab[1] = new Gtk::Label("Checking Account: ");
-	    ent[1] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[1]);
-	    lab[1]->show();
-	    ent[1]->set_text(default_text);
-	    ent[1]->set_max_length(50);
-	    ent[1]->show();
-	    dialog->get_vbox()->pack_start(*ent[1]);
+	    lab1[1] = new Gtk::Label("Checking Account: ");
+	    ent1[1] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[1]);
+	    lab1[1]->show();
+	    ent1[1]->set_text(default_text);
+	    ent1[1]->set_max_length(50);
+	    ent1[1]->show();
+	    dialog->get_vbox()->pack_start(*ent1[1]);
 
 
-	    lab[2] = new Gtk::Label("Savings Account: ");
-	    ent[2] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[2]);
-	    lab[2]->show();
-	    ent[2]->set_text(default_text);
-	    ent[2]->set_max_length(50);
-	    ent[2]->show();
-	    dialog->get_vbox()->pack_start(*ent[2]);
+	    lab1[2] = new Gtk::Label("Savings Account: ");
+	    ent1[2] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[2]);
+	    lab1[2]->show();
+	    ent1[2]->set_text(default_text);
+	    ent1[2]->set_max_length(50);
+	    ent1[2]->show();
+	    dialog->get_vbox()->pack_start(*ent1[2]);
 
-	    lab[3] = new Gtk::Label("Brokerage Accounts (stocks,etc.): ");
-	    ent[3] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[3]);
-	    lab[3]->show();
-	    ent[3]->set_text(default_text);
-	    ent[3]->set_max_length(50);
-	    ent[3]->show();
-	    dialog->get_vbox()->pack_start(*ent[3]);
-
-
-	    lab[4] = new Gtk::Label("Primary Residence: ");
-	    ent[4] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[4]);
-	    lab[4]->show();
-	    ent[4]->set_text(default_text);
-	    ent[4]->set_max_length(50);
-	    ent[4]->show();
-	    dialog->get_vbox()->pack_start(*ent[4]);
+	    lab1[3] = new Gtk::Label("Brokerage Accounts (stocks,etc.): ");
+	    ent1[3] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[3]);
+	    lab1[3]->show();
+	    ent1[3]->set_text(default_text);
+	    ent1[3]->set_max_length(50);
+	    ent1[3]->show();
+	    dialog->get_vbox()->pack_start(*ent1[3]);
 
 
-	    lab[5] = new Gtk::Label("Vehicle: ");
-	    ent[5] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[5]);
-	    lab[5]->show();
-	    ent[5]->set_text(default_text);
-	    ent[5]->set_max_length(50);
-	    ent[5]->show();
-	    dialog->get_vbox()->pack_start(*ent[5]);
+	    lab1[4] = new Gtk::Label("Primary Residence: ");
+	    ent1[4] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[4]);
+	    lab1[4]->show();
+	    ent1[4]->set_text(default_text);
+	    ent1[4]->set_max_length(50);
+	    ent1[4]->show();
+	    dialog->get_vbox()->pack_start(*ent1[4]);
+
+
+	    lab1[5] = new Gtk::Label("Vehicle: ");
+	    ent1[5] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[5]);
+	    lab1[5]->show();
+	    ent1[5]->set_text(default_text);
+	    ent1[5]->set_max_length(50);
+	    ent1[5]->show();
+	    dialog->get_vbox()->pack_start(*ent1[5]);
 
 	 
-	    lab[6] = new Gtk::Label("Other: ");
-	    ent[6] = new Gtk::Entry();
-	    dialog->get_content_area()->pack_start(*lab[6]);
-	    lab[6]->show();
-	    ent[6]->set_text(default_text);
-	    ent[6]->set_max_length(50);
-	    ent[6]->show();
-	    dialog->get_vbox()->pack_start(*ent[6]);
+	    lab1[6] = new Gtk::Label("Other: ");
+	    ent1[6] = new Gtk::Entry();
+	    dialog->get_content_area()->pack_start(*lab1[6]);
+	    lab1[6]->show();
+	    ent1[6]->set_text(default_text);
+	    ent1[6]->set_max_length(50);
+	    ent1[6]->show();
+	    dialog->get_vbox()->pack_start(*ent1[6]);
 
 	    //last
 	    dialog->add_button("Reset", 0);
@@ -307,7 +346,7 @@ static string * inputAssets(string title, string default_text="")
 		string * text = new string[7];
 		for(int i=0; i<7; i++)
 		{
-			text[i] = ent[i]->get_text();
+			text[i] = ent1[i]->get_text();
 
 			if(default_text == text[i])
 			{
@@ -324,23 +363,60 @@ static string * inputAssets(string title, string default_text="")
 	    delete dialog;
 		for(int i=0;i<7;i++)
 		{
-		    delete lab[i];
+		    delete lab1[i];
 
-		    delete ent[i];
+		    delete ent1[i];
 		}
 
-
-			/*ele = stof(text[0]);
-			water = stof(text[1]);
-			gas = stof(text[2]);
-			wifi = stof(text[3]);
-			phone = stof(text[4]);
-			insurance = stof(text[5]);
-			loansOther = stof(text[6]);*/
 			return text;
 
 }
 
+static Person inputInfo(string title, string default_text="")
+{
+	Dialog dialog;	
+	dialog.set_title(title);
+
+	dialog.add_button("Cancel", 0);
+	dialog.add_button("OK",1);
+	dialog.set_default_response(1);
+
+	Label label("Enter your name:");
+	dialog.get_content_area()->pack_start(label);
+	label.show();
+	Entry entryName;
+	entryName.set_text(default_text);
+	entryName.set_max_length(50);
+	entryName.show();
+	dialog.get_vbox()->pack_start(entryName);
+
+	Label label2("Enter your income:");
+	dialog.get_content_area()->pack_start(label2);
+	label2.show();
+	Entry entryIncome;
+	entryIncome.set_text(default_text);
+	entryIncome.set_max_length(50);
+	entryIncome.show();
+	dialog.get_vbox()->pack_start(entryIncome);
+
+	int result=dialog.run();
+	Person p1;
+	p1.name = entryName.get_text();
+	p1.income=stof(entryIncome.get_text());
+
+	dialog.close();
+	while(Main::events_pending())
+	{
+		Main::iteration();
+	}
+		
+	if(result == 1)
+		return p1;
+	else
+		Main::quit();
+
+	
+}
 
 //asks conformation question
   static int question(string msg,string title, vector<std::string> buttons)
--- Net-Worth-Calculator-master/proj.cpp
+++ Net-Worth-Calculator-master/proj.cpp
@@ -1,45 +1,73 @@
 #include "pro_gui.hpp"
 #include <iostream>
-
+#include <fstream>
+#include<iomanip>
 using namespace std;
 
 int main(int argc, char** argv){
 	Main kit(argc,argv);
+
+	Person p = Dialogs::inputInfo("Information");
+	p.fillLiaAss();
+
 	liability l;
-	
 	string * ret = Dialogs :: input("Liability (monthly)");
 
+	for(int i = 0; i<7;i++)
+	{
+		l.liaValue.push_back(stof(ret[i]));
+	}
 
-	l.ele = stof(ret[0]);
+	/*l.ele = stof(ret[0]);
 	l.water = stof(ret[1]);
 	l.gas = stof(ret[2]);
 	l.wifi = stof(ret[3]);
 	l.phone = stof(ret[4]);
 	l.insurance = stof(ret[5]);
-	l.loansOther = stof(ret[6]);
+	l.loansOther = stof(ret[6]);*/
 
 	double totalLia = l.total_lia();
 
 	Assets a;
 	string * retAsset= Dialogs::inputAssets("Asset(monthly)");
+	
+	for(int j = 0; j<7;j++)
+	{
+		a.assValue.push_back(stof(retAsset[j]));
+	}
 
-	a.cashOnHand= stof(retAsset[0]);
+	/*a.cashOnHand= stof(retAsset[0]);
 	a.checkingAccounts= stof(retAsset[1]);
 	a.savingsAccounts =stof(retAsset[2]);
 	a.MoneyMarkets=stof(retAsset[3]);
 	a.PrimaryResidence=stof(retAsset[4]);
 	a.vehicle=stof(retAsset[5]);
-	a.other = stof(retAsset[6]);
+	a.other = stof(retAsset[6]);*/
 
 	double totalAssets = a.total_assets();
 
-	double totalNet = totalAssets-totalLia;
+	double totalNet =p.income+ totalAssets-totalLia;
 
+	Dialogs::message("Here is your report: " + p.name + " with income of $" + to_string(p.income).substr(0,to_string(p.income).size()-4),"Thank you");
 	Dialogs::message("Total Liabilities:  $"+to_string(totalLia).substr(0, to_string(totalLia).size()-4), "Thank you!");
 	Dialogs::message("Total Assets: $ "+ to_string(totalAssets).substr(0, to_string(totalAssets).size()-4), "Thank you!");
 	Dialogs::message("Total Net Worth as of the current month: $" + to_string(totalNet).substr(0, to_string(totalNet).size()-4), "Thank you!");
 
+	//Place in function
+	ofstream report;
+	report.open("networth.txt");
+	report<<"\n****Liabilitities****\n\n";
+	for(int x= 0; x<p.liabilities.size();x++)
+	{
+		report << p.liabilities[x]<<l.liaValue[x]<<endl;
+	}
+	report<<"\n****Assets:****\n\n";
+	for(int x= 0; x<p.liabilities.size();x++)
+	{
+		report << p.assets[x]<<a.assValue[x]<<endl;
+	}
+	
+	report.close();
 	return 0;
-
 
 }
